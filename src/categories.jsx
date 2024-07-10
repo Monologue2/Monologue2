d334c59f268111b0a92591ba2f9d584b2767439e
shogle <seyoumoon@gmail.com>
2024-07-11 01:28:38 +0900

    categories.jsx

---
+++ src/categories.jsx
@@ -0,0 +1,42 @@
+
+export const Programming_Language = {
+    Expert: "Expert in : Go, Python",
+    Knowledgeable: "Knowledgeable in : C/C++, ShellScript, Java, JavaScript"
+};
+
+export const Web_Dev = {
+    Back: "Back-End : Fiber, Flask",
+    Front: "Front-End : React"
+};
+
+export const Database = {
+    SQL: "SQL : MySQL, MariaDB",
+    NoSQL: "NoSQL : MongoDB",
+    MQ: "MQ : Kafka, Mosquitto"
+};
+
+export const Containerize = {
+    Docker: "Docker",
+    K8s: "K8s"
+};
+
+export const Monitoring = {
+    Metrics: "Managing Metrics : Prometheus",
+    Observability: "Observability : Grafana"
+};
+
+export const CI_CD = {
+    Jenkins: "Jenkins",
+    GitAction: "Github Action"
+};
+
+export const Cloud_System_Administration = {
+    AWS: "AWS(Amazon Web Services) : EC2, MSK",
+    System: "System Administration : Linux(Ubuntu, Debian, Alpine)"
+};
+
+export const Productive_Tools = {
+    Remote: "Remote : SSH, RDP, VNC",
+    Package: "Package Manager : Anaconda, Gradle, NPM",
+    Editor: "ditor : VSC, Jupyter Lab"
+};
