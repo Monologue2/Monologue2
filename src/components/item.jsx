bf70d5c129a60541afead229425be000fd3abca2
shogle <seyoumoon@gmail.com>
2024-07-11 16:28:11 +0900

    item & skill

--- src/components/item.jsx
+++ src/components/item.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 export const LinkedItem = ({icon, content, link}) => {
     return (
-      <p className="relative self-stretch">
+      <p className="relative self-stretch mt-2">
           {link ?
               <a href={link} rel="noopener noreferrer" target="_blank">
                   <Item icon={icon} content={content}/>
@@ -16,7 +16,7 @@ export const LinkedItem = ({icon, content, link}) => {
 export const Item = ({icon, content}) => {
     return (
         <div className="item relative flex flex-row justify-start items-center"> 
-            {icon && <img className="w-[20px] h-[20px] opacity-80 mr-[5px] absolute top-0.5 left-0.5" alt="" src={icon}/>}
+            {icon && <img className="w-[18px] h-[18px] opacity-80 mr-[5px] absolute top-0.5 left-0.5" alt="" src={icon}/>}
             <span className="pl-[23.5px]" style={{ fontFamily: 'Pretendard Regular' }} > {content} </span>
         </div>
     )
--- src/components/skill.jsx
+++ src/components/skill.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import {Item} from './item.jsx'
 
-export const Skill = ({category, icon,...skill}) => {
+export const Skill = ({category,...skill}) => {
     return ( 
     <>
-        <div className="skill" style={{}}>
+        <div className="skill">
             <div className="category" style={{ fontFamily: 'Pretendard ExtraBold' }}>{category}</div>
             {Object.keys(skill).map((key) => (
-                <Item content={skill[key]} icon={icon}/>
+                <Item content={skill[key].content} icon={skill[key].icon}/>
             ))}
         </div>
     </>
