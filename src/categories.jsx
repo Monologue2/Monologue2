00348b2d8d7b61a2b120aa0a92e02c1ccb031755
shogle <seyoumoon@gmail.com>
2024-07-11 11:31:38 +0900

    categories

--- src/categories.jsx
+++ src/categories.jsx
@@ -1,42 +1,96 @@
 
 export const Programming_Language = {
-    Expert: "Expert in : Go, Python",
-    Knowledgeable: "Knowledgeable in : C/C++, ShellScript, Java, JavaScript"
+    Expert: {
+        content : "Expert in : Go, Python",
+        icon : "/img/icon/programming-language.png",
+    },
+    Knowledgeable: {
+        content : "Knowledgeable in : C/C++, ShellScript, Java, JavaScript", 
+        icon : "/img/icon/programming-language.png",
+    }
 };
 
 export const Web_Dev = {
-    Back: "Back-End : Fiber, Flask",
-    Front: "Front-End : React"
+    Back: {
+        content : "Back-End : Fiber, Flask",
+        icon : "/img/icon/backend-black.png",
+    },
+    Front: {
+        content : "Front-End : React",
+        icon : "/img/icon/frontend-black.png",
+    }
 };
 
 export const Database = {
-    SQL: "SQL : MySQL, MariaDB",
-    NoSQL: "NoSQL : MongoDB",
-    MQ: "MQ : Kafka, Mosquitto"
+    SQL: {
+        content : "SQL : MySQL, MariaDB",
+        icon : "/img/icon/sql-black.png",
+    },
+    NoSQL: {
+        content : "NoSQL : MongoDB",
+        icon : "/img/icon/not-only-sql.png",
+    },
+    MQ: {
+        content : "MQ : Kafka, Mosquitto",
+        icon : "/img/icon/message-queue-black.png",
+    },
 };
 
 export const Containerize = {
-    Docker: "Docker",
-    K8s: "K8s"
+    Docker: {
+        content : "Docker",
+        icon : "/img/icon/docker.png",
+    },
+    K8s: {
+        content : "K8s",
+        icon : "/img/icon/kubernetes.png",
+    }
 };
 
 export const Monitoring = {
-    Metrics: "Managing Metrics : Prometheus",
-    Observability: "Observability : Grafana"
+    Metrics: {
+        content : "Managing Metrics : Prometheus",
+        icon : "/img/icon/prometheus.png",
+    },
+    Observability: {
+        content : "Observability : Grafana",
+        icon : "/img/icon/grafana.png",
+    }
 };
 
 export const CI_CD = {
-    Jenkins: "Jenkins",
-    GitAction: "Github Action"
+    Jenkins: {
+        content : "Jenkins",
+        icon : "/img/icon/jenkins.png",
+    },
+    GitAction: {
+        content : "Github Action",
+        icon : "/img/icon/gitaction.png",
+    }
 };
 
 export const Cloud_System_Administration = {
-    AWS: "AWS(Amazon Web Services) : EC2, MSK",
-    System: "System Administration : Linux(Ubuntu, Debian, Alpine)"
+    AWS: {
+        content : "AWS(Amazon Web Services) : EC2, MSK",
+        icon : "/img/icon/aws.png",
+    },
+    System: {
+        content : "System Administration : Linux(Ubuntu, Debian, Alpine)",
+        icon : "/img/icon/linux.png",
+    }
 };
 
 export const Productive_Tools = {
-    Remote: "Remote : SSH, RDP, VNC",
-    Package: "Package Manager : Anaconda, Gradle, NPM",
-    Editor: "ditor : VSC, Jupyter Lab"
+    Remote: {
+        content : "Remote : SSH, RDP, VNC",
+        icon : "/img/icon/ssh.png",
+    },
+    Package: {
+        content : "Package Manager : Anaconda, NPM, Gradle",
+        icon : "/img/icon/conda.png",
+    },
+    Editor: {
+        content : "ditor : VSC, Jupyter Lab",
+        icon : "/img/icon/vsc.png",
+    }
 };
